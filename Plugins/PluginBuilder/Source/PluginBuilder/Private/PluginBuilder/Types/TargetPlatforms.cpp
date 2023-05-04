38a3e95a822487b6bbae74aeea0d1f7b56a63e9a
Naotsun <51815450+Naotsun19B@users.noreply.github.com>
2023-05-05 01:15:34 +0900

    Add files via upload

---
+++ Plugins/PluginBuilder/Source/PluginBuilder/Private/PluginBuilder/Types/TargetPlatforms.cpp
@@ -0,0 +1,99 @@
+﻿// Copyright 2022-2023 Naotsun. All Rights Reserved.
+
+#include "PluginBuilder/Types/TargetPlatforms.h"
+#include "PluginBuilder/PluginBuilderGlobals.h"
+#include "PluginBuilder/Utilities/PluginBuilderSettings.h"
+#include "Interfaces/ITargetPlatformManagerModule.h"
+#include "Interfaces/ITargetPlatform.h"
+#if UE_5_00_OR_LATER
+#include "Styling/AppStyle.h"
+#include "Misc/DataDrivenPlatformInfoRegistry.h"
+#else
+#include "EditorStyle.h"
+#include "PlatformInfo.h"
+#endif
+
+namespace PluginBuilder
+{
+	TArray<FTargetPlatforms::FTargetPlatform> FTargetPlatforms::GetPlatformNames(bool bWithRefresh)
+	{
+		if (bWithRefresh)
+		{
+			RefreshPlatformNames();
+		}
+		
+		return PlatformNames;
+	}
+
+	void FTargetPlatforms::RefreshPlatformNames()
+	{
+		ITargetPlatformManagerModule& TargetPlatformManagerRef = GetTargetPlatformManagerRef();
+		const TArray<ITargetPlatform*>& TargetPlatforms = TargetPlatformManagerRef.GetTargetPlatforms();
+		PlatformNames.Reset(TargetPlatforms.Num());
+		for (const ITargetPlatform* TargetPlatform : TargetPlatforms)
+		{
+			if (TargetPlatform == nullptr)
+			{
+				continue;
+			}
+
+			FTargetPlatform PlatformName;
+			{
+#if UE_5_00_OR_LATER
+				const FDataDrivenPlatformInfo& PlatformInfo = TargetPlatform->GetPlatformInfo();
+				PlatformName.UBTPlatformName = PlatformInfo.UBTPlatformString;
+				PlatformName.IniPlatformName = PlatformInfo.IniPlatformName.ToString();
+				PlatformName.PlatformGroupName = PlatformInfo.PlatformGroupName;
+#else
+				const PlatformInfo::FPlatformInfo& PlatformInfo = TargetPlatform->GetPlatformInfo();
+				PlatformInfo.UBTTargetId.ToString(UBTPlatformName);
+				IniPlatformName = PlatformInfo.IniPlatformName;
+#endif
+				if (PlatformInfo.PlatformSubMenu != NAME_None)
+				{
+					PlatformName.IniPlatformName = PlatformInfo.PlatformSubMenu.ToString();
+				}
+			}
+			
+			if (PlatformName.IniPlatformName.IsEmpty())
+			{
+				continue;
+			}
+
+			const FString& UBTPlatformName = PlatformName.UBTPlatformName;
+			const auto EqualsUBTPlatformName = [&UBTPlatformName](const FTargetPlatform& PlatformName) -> bool
+			{
+				return PlatformName.UBTPlatformName.Equals(UBTPlatformName);
+			};
+			if (PlatformNames.ContainsByPredicate(EqualsUBTPlatformName))
+			{
+				continue;
+			}
+			
+			PlatformNames.Add(PlatformName);
+		}
+	}
+
+	void FTargetPlatforms::ToggleTargetPlatform(const FTargetPlatform TargetPlatform)
+	{
+		auto& Settings = UPluginBuilderSettings::Get();
+		
+		if (Settings.TargetPlatforms.Contains(TargetPlatform.UBTPlatformName))
+		{
+			Settings.TargetPlatforms.Remove(TargetPlatform.UBTPlatformName);
+		}
+		else
+		{
+			Settings.TargetPlatforms.Add(TargetPlatform.UBTPlatformName);
+		}
+
+		Settings.SaveConfig();
+	}
+
+	bool FTargetPlatforms::GetTargetPlatformState(const FTargetPlatform TargetPlatform)
+	{
+		return UPluginBuilderSettings::Get().TargetPlatforms.Contains(TargetPlatform.UBTPlatformName);
+	}
+
+	TArray<FTargetPlatforms::FTargetPlatform> FTargetPlatforms::PlatformNames;
+}
---
+++ Plugins/PluginBuilder/Source/PluginBuilder/Private/PluginBuilder/Types/TargetPlatforms.h
@@ -0,0 +1,46 @@
+﻿// Copyright 2022-2023 Naotsun. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+namespace PluginBuilder
+{
+	/**
+	 * A class that manages the name of the platform to be built.
+	 */
+	class PLUGINBUILDER_API FTargetPlatforms
+	{
+	public:
+		// A structure that summarizes platform names in UBT and ini formats respectively.
+		struct FTargetPlatform
+		{
+		public:
+			// The name of the platform covered by UBT.
+			FString UBTPlatformName;
+
+			// The name of the platform covered by ini.
+			FString IniPlatformName;
+
+			// The identifier to group similar platforms together, such as "Mobile" and "Console".
+			FName PlatformGroupName;
+		};
+
+	public:
+		// Returns a list of available platform names.
+		static TArray<FTargetPlatform> GetPlatformNames(bool bWithRefresh = true);
+
+		// Collects available platform names from ITargetPlatformManagerModule.
+		static void RefreshPlatformNames();
+		
+		// Toggles selection state the specified target platform.
+		static void ToggleTargetPlatform(const FTargetPlatform TargetPlatform);
+
+		// Returns whether the specified target platform is selected.
+		static bool GetTargetPlatformState(const FTargetPlatform TargetPlatform);
+
+	private:
+		// A list of available platform names.
+		static TArray<FTargetPlatform> PlatformNames;
+	};
+}
