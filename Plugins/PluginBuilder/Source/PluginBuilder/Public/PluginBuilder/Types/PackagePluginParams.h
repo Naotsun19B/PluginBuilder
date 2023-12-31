79109280340c0d35ec8eb5fa892281006a5b095e
Naotsun <51815450+Naotsun19B@users.noreply.github.com>
2024-01-01 01:34:44 +0900

    Update PackagePluginParams.h

--- Plugins/PluginBuilder/Source/PluginBuilder/Public/PluginBuilder/Types/PackagePluginParams.h
+++ Plugins/PluginBuilder/Source/PluginBuilder/Public/PluginBuilder/Types/PackagePluginParams.h
@@ -46,11 +46,11 @@ namespace PluginBuilder
 	{
 	public:
 		// Whether to prevent editor platform compilation on the host.
-    	bool bNoHostPlatform;
+    		bool bNoHostPlatform;
     
-    	// The list of host platforms to build the plugin.
-    	// If nothing is specified, the current host platform is used.
-    	TArray<FString> HostPlatforms;
+    		// The list of host platforms to build the plugin.
+    		// If nothing is specified, the current host platform is used.
+    		TArray<FString> HostPlatforms;
     	
 		// The list of target platforms to build the plugin.
 		TArray<FString> TargetPlatforms;
